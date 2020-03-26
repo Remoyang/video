fb1cc90e72a4ed02e782797ecd0b33eac0d00987
v_dufyang <v_dufyang@tencent.com>
2020-03-27 00:43:27 +0800

    admin web init

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ app.debug = True
 from app.admin import admin as admin_blueprint
 from app.home import home as home_blueprint
 
-app.register_blueprint(admin_blueprint,url_prefix="/admin")
+app.register_blueprint(admin_blueprint, url_prefix="/admin")
 app.register_blueprint(home_blueprint)
 
 # 404
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,33 @@
+# -*- coding: utf8 -*-
 from . import admin
+from flask import render_template, redirect, url_for
 
+# 后端首页
 @admin.route("/")
 def index():
-    return "<h1 style='color:green'>admin</h1>"
+    return render_template("admin/index.html")
+
+# 后端登录
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+# 后端退出
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+# 后端修改密码
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
+
+# 后端添加标签
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+# 后端标签列表
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
